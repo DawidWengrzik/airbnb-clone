f0360130e439f051f72e35ba6ea104151016cb3c
Dawid Wengrzik <dewengrzik@gmail.com>
2023-06-08 18:50:57 +0200

    Added authorization

--- backend/routes/placesRoutes.js
+++ backend/routes/placesRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {getPlaces, setPlace, updatePlace, deletePlace} = require('../controllers/placeController')
+const { protect } = require('../middleware/authMiddleware')
 
 // getPlaces and postPlace route
-router.route('/').get(getPlaces).post(setPlace);
+router.route('/').get(protect, getPlaces).post(protect, setPlace);
 
 // put and delete Place route
-router.route('/:id').put(updatePlace).delete(deletePlace);
+router.route('/:id').put(protect, updatePlace).delete(protect, deletePlace);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/places', require('./routes/placesRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
