a708ecedeab94e1d3a2ba6d7548a534941507e46
Dawid Wengrzik <dewengrzik@gmail.com>
2023-06-20 23:27:51 +0200

    Updated: Authorization modal, added some styles, show trigger, visual changes

--- frontend/src/Components/AuthPopup/AuthorizationPopup.jsx
+++ frontend/src/Components/AuthPopup/AuthorizationPopup.jsx
@@ -1,49 +1,82 @@
 import './authPopupStyle.scss'
 import { GrClose } from 'react-icons/gr'
+import { AiFillFacebook, AiFillApple } from 'react-icons/ai';
 import { BiChevronDown } from 'react-icons/bi'
+import { FcGoogle } from 'react-icons/fc';
+import { FiMail } from 'react-icons/fi'
+import { useEffect, useState } from 'react';
 
-const AuthorizationPopup = () => {
-  return (
-    <div className="popup-container">
-        <div className="popup-form">
-            <header className="form-header">
-                <div className='header-close'> 
-                  <button className='header-close__btn'><GrClose /></button>
+const AuthorizationPopup = ({ showModal, setShowModal }) => {
+
+  if (showModal) {
+    return (
+      <div className="popup-container">
+          <div className="popup-form">
+              <header className="form-header">
+                  <div className='header-close'> 
+                    <button className='header-close__btn' onClick={() => setShowModal(!showModal)}><GrClose /></button>
+                  </div>
+                  <span>Zaloguj się lub zarejestruj</span>
+              </header>
+              <hr className='separator' /> 
+              <div className="form-content">
+                <div className='form-content__header'>
+                  <h3>Witaj w Airbnb</h3>
                 </div>
-                <span>Zaloguj się lub zarejestruj</span>
-            </header>
-            <hr className='separator' /> 
-            <div className="form-content">
-              <div className='form-content__header'>
-                <h3>Witaj w Airbnb</h3>
-              </div>
-                
-              <div className='form-data'>
-                <div id='country-select' className='form-login__select'> 
-                <div className='select-label'>
-                  <label for='country-select'>Kraj/region</label>
-                </div>
-                  <select>
-                    <option value="0">Polska (+48)</option>
-                  </select>
                   
-                  <div className='arrow-down'>
-                    <BiChevronDown className='arrow-icon'/>
+                <div className='form-data'>
+                  <div id='country-select' className='form-login__select'> 
+                  <div className='select-label'>
+                    <label for='country-select'>Kraj/region</label>
+                  </div>
+                    <select>
+                      <option value="0">Polska (+48)</option>
+                    </select>
+                    
+                    <div className='arrow-down'>
+                      <BiChevronDown className='arrow-icon'/>
+                    </div>
+                  </div>
+                  
+                  <div className='form-login__input'>
+                    <div className='login-number'>
+                      <span>+48</span>
+                    </div>
+                    <input type="text" placeholder='Numer telefonu' className='login-input'/>
                   </div>
                 </div>
-                
-                <span className='form-login__input'></span>
+                <div className='from-login__container'>
+                  <span className='form-login__info'>Zadzwonimy lub wyślemy SMS-a, aby potwierdzić Twój numer. 
+                    Obowiązują standardowe opłaty za wysyłanie wiadomości i transmisję danych.
+                    &nbsp;<a href="https://www.airbnb.pl/help/article/2855" target="_blank" className='info-highlight'>Polityka Prywatności</a></span>
+                </div>
+                <button className='form-button'>
+                  <span>Dalej</span>
+                </button>
+                <div className='content-separator'>lub</div>
+                <div className='login-options'>
+                  <div className='login-option'>
+                    <AiFillFacebook className='login-option__icon' style={{fill: "#4267B2"}}/>
+                    <span>Kontynuuj, używając Facebook</span>
+                  </div>
+                  <div className='login-option'>
+                    <FcGoogle className='login-option__icon' />
+                    <span>Kontynuuj, używając Google</span>
+                  </div>
+                  <div className='login-option'>
+                    <AiFillApple className='login-option__icon' />
+                    <span>Kontynuuj przez Apple</span>
+                  </div>
+                  <div className='login-option'>
+                    <FiMail className='login-option__icon' />
+                    <span>Użyj adresu email</span>
+                  </div>
+                </div>
               </div>
-              <div>
-                <span className='form-login__info'>Zadzwonimy lub wyślemy SMS-a, aby potwierdzić Twój numer. 
-                  Obowiązują standardowe opłaty za wysyłanie wiadomości i transmisję danych.
-                  &nbsp;<a href="https://www.airbnb.pl/help/article/2855" target="_blank" className='info-highlight'>Polityka Prywatności</a></span>
-              </div>
-              
-            </div>
-        </div>
-    </div>
-  )
+          </div>
+      </div>
+    )
+  }
 }
 
 export default AuthorizationPopup
--- frontend/src/Components/Nav/UserPanel.js
+++ frontend/src/Components/Nav/UserPanel.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import {HiOutlineGlobeAlt} from 'react-icons/hi'
 import {FaUser} from 'react-icons/fa'
+import AuthorizationPopup from '../AuthPopup/AuthorizationPopup';
 
 const UserPanel = () => {
 
     const [isDropdownActive, setIsDropdownActive] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     return (
+        <>
+        {showModal && <AuthorizationPopup showModal={showModal} setShowModal={setShowModal}/>}
         <div className='user-panel'>
             <button className='user-panel__btn'>
                 <span>Wynajmij swój dom na Airbnb</span>
@@ -23,14 +27,16 @@ const UserPanel = () => {
                 </span>
                 {isDropdownActive && 
                     (<div className='user-panel__dropdown'>
-                        <span className='dropdown-span'>Zarejestruj się</span>
-                        <span className='dropdown-span'>Zaloguj się</span>
+                        <a onClick={() => setShowModal(!showModal)}><span className='dropdown-span'>Zarejestruj się</span></a>
+                        <a onClick={() => setShowModal(!showModal)}><span className='dropdown-span'>Zaloguj się</span></a>
                         <hr className='dropdown-separator'/>
                         <span className='dropdown-span'>Wynajmij swój dom na Airbnb</span>
-                    <span className='dropdown-span'>Pomoc</span>
-                </div>)}
+                        <span className='dropdown-span'>Pomoc</span>
+                    </div>)}
             </button>
         </div>
+        </>
+        
     )
 }
 
